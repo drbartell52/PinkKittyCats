3818fbcee53ea16e8563a03d8a89ab2062ae3498
sunflowers33d <37161028+sunflowers33d@users.noreply.github.com>
2023-02-04 00:18:13 -0500

    collider fix, super basic skeleton of gameplay now present
    the reason the waterdrops were getting stuck was because of an extra Polygon Collider 2D...and basic idea of how the game will play out is now present

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,7 @@
+public enum GameState
+{
+    Playing, // 0
+    Won, // 1
+    Lost, // 2
+    Paused // 3
+}
---
+++ Assets/Scripts/Waterdrop.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waterdrop : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
