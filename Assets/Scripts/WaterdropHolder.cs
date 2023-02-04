9d19de0ecea527e1bd7476cb4c9092b449538c4c
sunflowers33d <37161028+sunflowers33d@users.noreply.github.com>
2023-02-04 01:23:05 -0500

    added controllable "waterdrop holder"
    trying to make the gameplay more present, still pretty messy but is somewhat more coherent now

---
+++ Assets/Scripts/WaterdropHolder.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterdropHolder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // JUST FOR TESTING
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }    
+    }
+    
+}
